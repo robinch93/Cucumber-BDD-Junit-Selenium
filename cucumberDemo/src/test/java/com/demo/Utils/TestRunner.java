b944b78cb50a82f4d389fff54ba274115688da14
robin <34096241+robinch93@users.noreply.github.com>
2021-12-19 22:35:15 +0100

    refactors

--- cucumberDemo/src/test/java/com/demo/Utils/TestRunner.java
+++ cucumberDemo/src/test/java/com/demo/Utils/TestRunner.java
@@ -8,11 +8,7 @@ import io.cucumber.junit.CucumberOptions;
 @RunWith(Cucumber.class)
 @CucumberOptions(features = "src/test/java/com/demo/features", glue = { "com.demo.stepDefinition" }, plugin = {
 		"pretty", "html:test-results/HtmlReports/report.html", "json:test-results/JsonReports/report.json",
-		"junit:test-results/JUnitReports/report.xml" }, monochrome = true,
-		// strict = true, // it will check if any step is not defined in step
-		// definition.
-		dryRun = false // to check if the mapping is proper between feature and step definition file.
-)
+		"junit:test-results/JUnitReports/report.xml" }, monochrome = true, dryRun = false, tags = "@SmokeTest and @EndtoEndTest and not @RegressionTest")
 
 public class TestRunner {
 
